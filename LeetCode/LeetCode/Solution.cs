edb9fad07e17bdb2aa4b5faa2969a4a9128abba3
JiaSyuan Li <jiasyuan@hotmail.com>
2019-03-21 00:58:14 +0800

    TwoSum

---
+++ LeetCode/LeetCode/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var mapping = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var key = target - nums[i];
+                if (mapping.ContainsKey(key))
+                {
+                    return new int[] { mapping[key], i };
+                }
+                else
+                {
+                    if (!mapping.ContainsKey(nums[i]))
+                    {
+                        mapping.Add(nums[i], i);
+                    }
+                }
+            }
+            throw new Exception("target not found.");
+        }
+    }
+}
