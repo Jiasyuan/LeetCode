c2dc6f5f192864b0428b4b3afcf4082fdac1fc74
JiaSyuan Li <jiasyuan@hotmail.com>
2019-03-21 01:06:46 +0800

    AddTwoNumbers

---
+++ LeetCode/LeetCode/Definition/ListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Definition
+{
+
+    /// <summary>
+    /// Definition for singly-linked list.
+    /// </summary>
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+}
--- LeetCode/LeetCode/Solution.cs
+++ LeetCode/LeetCode/Solution.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LeetCode.Definition;
 
 namespace LeetCode
 {
     public class Solution
     {
+        /// <summary>
+        /// TwoSum
+        /// </summary>
+        /// <param name="nums">Given </param>
+        /// <param name="target"></param>
+        /// <returns></returns>
         public int[] TwoSum(int[] nums, int target)
         {
             var mapping = new Dictionary<int, int>();
@@ -27,6 +34,37 @@ namespace LeetCode
                 }
             }
             throw new Exception("target not found.");
+        }
+
+        /// <summary>
+        /// AddTwoNumbers
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            int x = 0;
+            ListNode p, dummy = new ListNode(0);
+            p = dummy;
+            while (l1 != null | l2 != null | x != 0)
+            {
+                if (l1 != null)
+                {
+                    x += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                {
+                    x += l2.val;
+                    l2 = l2.next;
+                }
+                p.next = new ListNode(x % 10);
+                x = x / 10;
+                p = p.next;
+            }
+            return dummy.next;
+
         }
     }
 }
